fcef6ce7b6cf4c875aed89720574b4cee86f73d3
wonhecho <chowond02@naver.com>
2021-08-17 21:17:53 +0900

    first commit

---
+++ src/main/java/net/cho/api/customer/CustomRepository.java
@@ -0,0 +1,8 @@
+package net.cho.api.customer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomRepository extends MongoRepository<Customer,Long> {
+}
